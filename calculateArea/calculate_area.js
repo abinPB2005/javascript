a5174d2c605bf0ffe699ed8a95f9b656025e5540
abin <abin1362005@gmail.com>
2026-05-23 04:30:49 -0400

    message

--- calculateArea/calculate_area.js
+++ calculateArea/calculate_area.js
@@ -7,8 +7,6 @@ function calculateArea(){
 
 let area = length * width
 document.getElementById('result').innerText = `the area of the reactangle is: ${area}`;
-<<<<<<< HEAD
-}
-=======
-}
->>>>>>> a46251b03a15a90bdd2262c441e8bcaa135881f4
+
+
+
